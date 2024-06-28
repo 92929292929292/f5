58d7c2ec6a803d47f6ced54eafa23535a918d98b
admin <admin@YD01-13>
2024-06-28 16:32:44 +0900

    fff

--- strongRefresh/src/main/java/co/sf/FrontController.java
+++ strongRefresh/src/main/java/co/sf/FrontController.java
@@ -26,6 +26,7 @@ public class FrontController extends HttpServlet{
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		map.put("/main.do", new MainControl());
+		map.put("/branch.do", new Branch());
 	}
 	
 	@Override
